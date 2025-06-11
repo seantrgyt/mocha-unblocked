ce02c90b7469a4538ad2a59cd8887a5ba3b2e0da
seantrgyt <94722625+seantrgyt@users.noreply.github.com>
2025-06-11 13:57:49 -0400

    Update home.tsx

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -14,13 +14,13 @@ export default function Home() {
       <div class="absolute left-1/2 top-1/2 flex w-screen -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-4">
         <div class="flex items-center gap-3">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-12 w-12">
-            <title>Mocha icon</title>
+            <title>SMS Unblocker Logo</title>
             <path
               fill="currentColor"
               d="M88 0C74.7 0 64 10.7 64 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C120.5 112.3 128 119.9 128 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C119.5 47.7 112 40.1 112 24c0-13.3-10.7-24-24-24zM32 192c-17.7 0-32 14.3-32 32V416c0 53 43 96 96 96H288c53 0 96-43 96-96h16c61.9 0 112-50.1 112-112s-50.1-112-112-112H352 32zm352 64h16c26.5 0 48 21.5 48 48s-21.5 48-48 48H384V256zM224 24c0-13.3-10.7-24-24-24s-24 10.7-24 24c0 38.9 23.4 59.4 39.1 73.1l1.1 1C232.5 112.3 240 119.9 240 136c0 13.3 10.7 24 24 24s24-10.7 24-24c0-38.9-23.4-59.4-39.1-73.1l-1.1-1C231.5 47.7 224 40.1 224 24z"
             />
           </svg>
-          <h1 class="text-5xl font-semibold">Mocha</h1>
+          <h1 class="text-5xl font-semibold">SMS Unblocker</h1>
         </div>
         <div class="join w-1/3">
           <input
@@ -41,7 +41,7 @@ export default function Home() {
       </div>
 
       <div class="absolute bottom-0 flex w-screen items-center justify-between p-4 px-6 text-sm">
-        &copy; 2025 proudparrot2
+        &copy; 2025 seantrg
         <div class="flex gap-4">
           <div class="flex items-center gap-2">
             <GitCommitHorizontal />
@@ -50,19 +50,7 @@ export default function Home() {
             </A>
           </div>
           <Dot class="-mx-3" />
-          <a class="link-hover link" href="https://github.com/cafe-labs/mocha" rel="noreferrer" target="_blank">
-            GitHub
           </a>
-          <button
-            class="link-hover link p-0 m-0 -mt-1"
-            type="button"
-            onClick={() => {
-              const modal = document.querySelector('#discordmodal') as HTMLDialogElement
-              modal.showModal()
-            }}
-          >
-            Discord
-          </button>
           <A href="/faq" class="link-hover link">
             FAQ
           </A>
