dbefa823b4297874eb2b74cf9b2fcb335f0d1a62
SeanTRG <94722625+seantrgyt@users.noreply.github.com>
2025-08-17 18:13:40 -0400

    Update aboutblank.ts

--- src/lib/aboutblank.ts
+++ src/lib/aboutblank.ts
@@ -4,11 +4,6 @@ import type { AboutBlankData } from './types'
 export function handleAboutBlank() {
   const aboutblankData = store('aboutblank') as AboutBlankData
 
-  if (aboutblankData.enabled && window.self === window.top) {
-    openAbWindow(window.location.origin)
-  }
-}
-
 export function openAbWindow(src: string, redirect = true) {
   const tab = window.open('about:blank', '_blank')
   if (!tab) return
